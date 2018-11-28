ade7ac2649aa4822e15c2344ff0342272e2610d5
JieunSummerPark <parkj2@student.swosu.edu>
2018-11-28 12:41:17 -0600

    Upload

--- Plinko_Team_2/src/Plinko_Joey_01.java
+++ Plinko_Team_2/src/Plinko_Joey_01.java
@@ -1,9 +1,10 @@
-
 public class Plinko_Joey_01 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		for(int a = 1; a < 20; a ++) {
+			for (int b = 0; b < a+1; b++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
 	}
-
 }
